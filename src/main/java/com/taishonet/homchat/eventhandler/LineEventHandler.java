422950bee991d5e3f776118497e2cad8ee1b8fb3
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2017-05-16 23:25:11 +0900

    雑にコマンド火

--- src/main/java/com/taishonet/homchat/eventhandler/LineEventHandler.java
+++ src/main/java/com/taishonet/homchat/eventhandler/LineEventHandler.java
@@ -9,6 +9,8 @@ import com.taishonet.homchat.repository.TrelloRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
+
 @LineMessageHandler
 public class LineEventHandler {
 
@@ -21,7 +23,15 @@ public class LineEventHandler {
 
     @EventMapping
     public TextMessage receiveMessage(MessageEvent<TextMessageContent> event) {
-        String registeredUrl = repository.registerNewDateCardToTrello(event.getMessage().getText());
-        return new TextMessage("きゅいきゅい！ つ" + registeredUrl);
+        String message = event.getMessage().getText();
+        if (!message.contains("追加")) {
+            return new TextMessage("きゅいきゅい！");
+        }
+        String[] commands = message.split("追加 ");
+        if (commands.length == 2) {
+            String registeredUrl = repository.registerNewDateCardToTrello(commands[1]);
+            return new TextMessage("きゅいきゅい！ つ" + registeredUrl);
+        }
+        return new TextMessage("きゅいきゅい！");
     }
 }
