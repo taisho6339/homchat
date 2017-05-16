1dd1e72cd99567a54aaed207b812504e67117816
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2017-05-16 22:50:47 +0900

    fix post requesst

--- src/main/java/com/taishonet/homchat/repository/TrelloRepository.java
+++ src/main/java/com/taishonet/homchat/repository/TrelloRepository.java
@@ -3,6 +3,8 @@ package com.taishonet.homchat.repository;
 import com.taishonet.homchat.utils.RequestUtils;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.net.URISyntaxException;
 
@@ -13,11 +15,16 @@ public class TrelloRepository {
     private static final String TRELLO_TOKEN = "100a24d0bcc73036ff180cade8007a373115ceb605225b948f4cd24d43c2db44";
     private static final String TRELLO_TARGET_LIST_ID = "591af9ccf8b507be793d546c";
 
-    private static final String TRELLO_ADD_CARD_API = "https://trello.com/1/cards?key=8b4c424f78a2695f0f51fd97cc513a56&token=100a24d0bcc73036ff180cade8007a373115ceb605225b948f4cd24d43c2db44&idList=591af9ccf8b507be793d546c&name=";
+    private static final String TRELLO_ADD_CARD_API = "https://api.trello.com/1/cards/";
 
     public void addNewDateCardToTrello(String name) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.set("key", TRELLO_KEY);
+        params.set("token", TRELLO_TOKEN);
+        params.set("idList", TRELLO_TARGET_LIST_ID);
+        params.set("name", name);
         try {
-            String body = RequestUtils.post(TRELLO_ADD_CARD_API + name, null, null);
+            String body = RequestUtils.post(TRELLO_ADD_CARD_API + name, null, params);
             System.out.println(body);
         } catch (URISyntaxException e) {
             e.printStackTrace();
