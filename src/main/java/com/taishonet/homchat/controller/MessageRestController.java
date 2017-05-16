3a9ca54d8639541a22a5d405df18510f5d886fff
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2017-05-16 09:37:58 +0900

    Revert "sdkを使ってみる"
    This reverts commit c3100601a37549a7f03a2d7a5b88a39533c14a3d.

--- src/main/java/com/taishonet/homchat/controller/MessageRestController.java
+++ src/main/java/com/taishonet/homchat/controller/MessageRestController.java
@@ -1,10 +1,5 @@
 package com.taishonet.homchat.controller;
 
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import com.taishonet.homchat.dto.EventPart;
 import com.taishonet.homchat.dto.LineEvent;
 import com.taishonet.homchat.repository.LineRepository;
@@ -24,23 +19,19 @@ import java.util.List;
  * Created by taisho6339 on 2017/05/13.
  */
 @RestController
-//@RequestMapping("/message")
-@LineMessageHandler
+@RequestMapping("/message")
 public class MessageRestController {
 
-//    @Autowired
-//    private LineRepository lineRepository;
+    @Autowired
+    private LineRepository lineRepository;
 
-    //    @RequestMapping(path = "/receive", method = RequestMethod.POST)
-//    @ResponseStatus(HttpStatus.ACCEPTED)
-    @EventMapping
-    public TextMessage index(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage("きゅいきゅい！");
-//        List<EventPart> eventPartList = lineEvent.getEvents();
-//        eventPartList.stream()
-//                .filter(eventPart -> !(StringUtils.isEmpty(eventPart.getReplyToken())))
-//                .forEach(eventPart -> lineRepository.reply(eventPart.getReplyToken()));
+    @RequestMapping(path = "/receive", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void index(@RequestBody LineEvent lineEvent) {
+        List<EventPart> eventPartList = lineEvent.getEvents();
+        eventPartList.stream()
+                .filter(eventPart -> !(StringUtils.isEmpty(eventPart.getReplyToken())))
+                .forEach(eventPart -> lineRepository.reply(eventPart.getReplyToken()));
         //TODO: Trelloに投稿する
         //TODO: 投稿したことをLINEに通知する
     }
