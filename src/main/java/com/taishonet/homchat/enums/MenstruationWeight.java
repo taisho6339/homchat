a676755e2a313b32541abdb9b682e737bccf3b84
坂本大将 <taisho6339@sakamototaishou-no-MacBook-Pro.local>
2017-06-27 23:45:06 +0900

    もらった文言を反映

--- src/main/java/com/taishonet/homchat/enums/MenstruationWeight.java
+++ src/main/java/com/taishonet/homchat/enums/MenstruationWeight.java
@@ -8,7 +8,11 @@ import lombok.Getter;
 public enum MenstruationWeight {
     WEIGHT_SMALL(0, "弱", "弱だよ"),
     WEIGHT_MEDIUM(1, "中", "中だよ"),
-    WEIGHT_LARGE(2, "強", "強だよ");
+    WEIGHT_LARGE(2, "強", "今回の生理は、かなり情緒不安定！！ささいなことで泣きたくなったりイライラしちゃうかも…\n" +
+            "ななさは、生理の時のルールをしっかり守ろう！なるべく心も体も無理をしない\n" +
+            "ひろき\n" +
+            "ななさはマトモじゃないという前提でいよう\n" +
+            "揉めそうになったときこそ、冷静になってハンドルを握ろう\n");
 
     private int code;
     private String label;
